4b1646a60c290799005d28c4957337a09cfd6ee0
Mirdalan <mirdalan.p@gmail.com>
2022-11-01 19:33:20 +0300

    вывод булева значений теперь с маленькой буквы

--- make_diff.py
+++ make_diff.py
@@ -1,14 +1,23 @@
 import json
 
 
+def data_type_check(value):
+    if isinstance(value, bool):
+        if value == None:
+            return 'null'
+        else:
+            return str(value).lower()
+    else:
+        return value
+
 def key_set(source):
     return ({x for x in source})
 
 def value_comparison(value, value_1, value_2):
     if value_1[value] == value_2[value]:
-        return f"    {value}: {value_1[value]}\n"
+        return f"    {value}: {data_type_check(value_1[value])}\n"
     else:
-        return f"  - {value}: {value_1[value]}\n  + {value}: {value_2[value]}\n"
+        return f"  - {value}: {data_type_check(value_1[value])}\n  + {value}: {data_type_check(value_2[value])}\n"
 
 def generate_diff(file_path1, file_path2):
     first = json.load(open(file_path1))
@@ -18,9 +27,9 @@ def generate_diff(file_path1, file_path2):
     result ='{\n'
     for value in dict_keys:
         if value in first and value not in second:
-            result += f"  - {value}: {first[value]}\n"
+            result += f"  - {value}: {data_type_check(first[value])}\n"
         elif value in second and value not in first:
-            result += f"  + {value}: {second[value]}\n"
+            result += f"  + {value}: {data_type_check(second[value])}\n"
         elif value in first and value in second:
             result += value_comparison(value, first, second)
     
