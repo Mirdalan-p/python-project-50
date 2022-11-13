22c88e5b2acaac53311c2ed6ef3fdedabe99d43b
Mirdalan <mirdalan.p@gmail.com>
2022-11-13 15:57:36 +0300

    gendiff fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,18 +1,16 @@
 import argparse
-import json
+from gendiff.scripts.make_diff import generate_diff
 
 
 parser = argparse.ArgumentParser(
     description='Compares two configuration files and shows a difference.')
-parser.add_argument('-f FORMAT', dest='--format FORMAT', default=json,
-                    help="set format of output")
+parser.add_argument('-f', '--format', help="set format of output")
 
 parser.add_argument('first_file')
 parser.add_argument("second_file")
 
 args = parser.parse_args()
 
-print(args)
 
-if __name__ == '__main__':
-    pass
+def main():
+    print(generate_diff(args.first_file, args.second_file))
