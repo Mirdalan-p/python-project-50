f717520a9d46fb934a5a32299d4834260a53edae
Mirdalan <mirdalan.p@gmail.com>
2022-12-06 19:55:57 +0300

    fixed

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,6 @@
 from gendiff.engine import generate_diff
 
 
-
 json_ = ('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
 yaml_ = ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
 mixed = ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml')
@@ -10,6 +9,7 @@ mixed = ('tests/fixtures/file1.json', 'tests/fixtures/file2.yml')
 def get_result(file):
     with open(file) as expected_result:
         return expected_result.read()
+
 
 def test_generate_diff_json():
     assert generate_diff(json_[0], json_[1], 'stylish') == \
@@ -27,6 +27,7 @@ def test_generate_diff_yaml():
            get_result('tests/fixtures/result_plain')
     assert generate_diff(yaml_[0], yaml_[1], 'json') == \
            get_result('tests/fixtures/result.json')
+
 
 def test_generate_diff_mixed():
     assert generate_diff(mixed[0], mixed[1], 'stylish') == \
