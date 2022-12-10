bdfee1ba4542a47db73d44692b5d096a1873575a
Mirdalan <mirdalan.p@gmail.com>
2022-12-10 15:12:19 +0300

    linter fixes

--- gendiff/comparer.py
+++ gendiff/comparer.py
@@ -2,7 +2,6 @@ from gendiff.scripts.parser import make_parse
 from gendiff.formatters.formatter import make_formatted
 
 
-
 def generate_diff(filepath_1, filepath_2, formatter='stylish'):
     data = (make_parse(filepath_1), make_parse(filepath_2))
     return make_formatted(data, formatter)
